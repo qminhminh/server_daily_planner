1aef67df801f536264dd13751e734d72ea6b5923
qminhminh <hqminh050503@gmail.com>
2024-09-24 20:13:40 +0700

    connect mongose

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();  // Load environment variables
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log("connected to the db"))
+    .catch((err) => console.log(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+
+const ip = "192.168.137.1";
+const port = process.env.PORT || 3000; 
+
+app.listen(port, () => {
+  console.log(`Product server listening on ${port}`);
+});
